161a8e7841ca215dea2669c33274dca139c19f7f
Eraght <eraght@gmail.com>
2023-05-30 13:13:34 +0600

    Yo

---
+++ README.md
@@ -0,0 +1,1 @@
+# mashinabishkek
--- parsing_mashina_kg.py
+++ parsing_mashina_kg.py
@@ -1,39 +1,3 @@
-
-
-# source = requests.get("https://www.mashina.kg/search/all/?page=1").text
-# soup = BS(source, "lxml")
-
-
-
-
-
-# info = soup.find_all("div", class_="list-item list-label")
-# dict_ = {}
-# print(info)
-# for i in info:
-#     title = i.find("h2", class_="name").text.strip()
-#     price = i.find("strong").text
-#     image = i.find("img", class_="lazy-image").get("data-src")
-#     description = (
-#         i.find("div", class_="block info-wrapper item-info-wrapper")
-#         .text.replace("\n", "")
-#         .replace(" ", "")
-#         .replace(",", " ")
-#     )
-#     dict_ = {"title": title, "price": price, "image": image, "description": description}
-#     write_to_csv(dict_)
-
-# засунь в функции типа этой 
-
-# def parse_multiple_pages(start_page, end_page):
-#     for page in range(start_page, end_page + 1):
-#         url = f"http://kenesh.kg/ru/news/all/list?page={page}"
-#         response = requests.get(url).text
-#         soup = BS(response, "lxml")
-#         process_info(soup) 
-        
-# и спарсь 5 страниц
-
 import requests
 from bs4 import BeautifulSoup as BS
 import csv
